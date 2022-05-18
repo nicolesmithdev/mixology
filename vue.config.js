1949ebba39160aa72fb8af34f4936976a974d145
nicolesmithweb <nicolesmithweb@me.com>
2022-05-18 19:01:36 -0400

    Configure Webpack alias

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
     publicPath: '/mixology/',
     transpileDependencies: true,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
+        },
+    },
 });
