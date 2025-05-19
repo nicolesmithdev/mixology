7d75ad3ed6bc1a6e45f1639a40b34c449377321a
Nicole Smith <nicolesmith@mac.lan>
2025-05-19 05:20:07 -0400

    Move stylesheet import from .Vue file to starting .js files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import store from './store';
 import App from './App.vue';
 import './registerServiceWorker'
+import './scss/style.scss';
 
 const app = createApp(App);
 
